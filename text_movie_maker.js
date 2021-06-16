8cb4330af002646ac8764cdab88a71f288d41a6e
maratkaSharp <marat2255a@gmail.com>
2021-06-16 19:05:27 +0300

    1.0

---
+++ text_movie_maker.js
@@ -0,0 +1,39 @@
+class Frame {
+    content;
+    classList;
+
+    constructor(content, classList) {
+        this.content = content;
+        this.classList = classList;
+    }
+}
+
+class Movie {
+    movieElem
+    frameDuration
+    allFrames
+    activeFrame
+    activeFrameIndex = 0;
+    
+    constructor(movieID, frameDuration, framesList) {
+        this.movieElem     = document.getElementById(movieID);
+        this.frameDuration = frameDuration;
+        this.allFrames     = framesList;
+        this.activeFrame   = framesList[this.activeFrameIndex];
+
+        this.movieLoop()
+    }
+
+    movieLoop () {
+        setTimeout(() => {
+            this.movieElem.classList = this.activeFrame.classList;
+            this.movieElem.innerHTML = this.activeFrame.content;
+    
+            if (this.activeFrameIndex != this.allFrames.length-1) {
+                this.activeFrameIndex++;
+                this.activeFrame = this.allFrames[this.activeFrameIndex];
+                this.movieLoop();
+            }
+        }, this.frameDuration * Math.min(this.activeFrameIndex, 1));
+    }
+}
